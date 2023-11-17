e771f44480e653c131fa3e897fa9ab16cf08a406
Ivan Tolstov <doc830@icloud.com>
2023-11-17 16:08:33 +0300

    Bug fixes

--- app.js
+++ app.js
@@ -8,16 +8,21 @@ app.use('/', require('./routes/routes'))
 const {SerialPort} =  require("serialport")
 const {UBXParser} = require('ubx-parser')
 const parser = new UBXParser();
-const port = new SerialPort({
-    path: '/dev/ttyS1',
-    baudRate: 115200
-})
 
 async function start() {
     try {
+        const port = new SerialPort({
+            path: config.get('serialPort'),
+            baudRate: config.get('baudRate')
+        })
         server.listen(PORT, () => console.log(`Server has been started on port ${PORT}...`))
         port.on("data", (buffer) => parser.parse(buffer));
-        parser.on("data", (data) => console.log(data));
+        parser.on("data", (data) => {
+            app.get('/', function (req,res){
+                res.send("<html lang=\"en\"><body>"+data+"</body></html>");
+            });
+            console.log(data)
+        })
     } catch (e) {
         console.log('Server error ', e.message);
         process.exit(1);
