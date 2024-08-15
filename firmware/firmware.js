7964241086e10dc742604a68a549d92c3eeaf65e
Tolstov Ivan <doc830@icloud.com>
2024-08-15 14:59:15 +0300

    Add server

--- firmware/firmware.js
+++ firmware/firmware.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const axios = require('axios')
 const {UBXParser} = require('ubx-parser')
 const {SerialPort} = require("serialport")
 const {ReadlineParser} = require('@serialport/parser-readline')
@@ -24,24 +24,23 @@ function firmware() {
     })
 //send UBX
     ubxParser.on("data", async (data) => {
-        let request = await http.request(
-            config.get('gw')
-            + '?itow=' + data["iTOW"]
-            + '&relPosN=' + data["relPosN"]
-            + '&relPosE=' + data["relPosE"]
-            + '&relPosD=' + data["relPosD"]
-            + '&relPosH=' + data["relPosHeading"]
-            + '&length=' + data["relPosLength"]
-            + '&isFix=' + data["diffFixOK"]
-            + '&diffSol=' + data["diffSoln"]
-            + '&carrSol=' + data["carrSoln"]
-            + '&r_timestamp=' + TIMESTAMP
-            + '&roverID=' + config.get('roverID')
-        )
-        request.on('error', () => {
-            console.log('Error with connection to investigator via ' + config.get('gw'))
+        await axios.post(config.get('gw'), {
+            itow: data["iTOW"],
+            relPosN: data["relPosN"],
+            relPosE: data["relPosE"],
+            relPosD: data["relPosD"],
+            relPosH: data["relPosHeading"],
+            length: data["relPosLength"],
+            isFix: data["diffFixOK"],
+            diffSol: data["diffSoln"],
+            carrSol: data["carrSoln"],
+            r_timestamp: TIMESTAMP,
+            roverID: config.get('roverID')
         })
-        request.end()
+            .then((res) => {})
+            .catch((error) => {
+            console.error('Post request error:', error);
+        })
     })
 //catch NMEA
     const nmeaParser = serialPort.pipe(new ReadlineParser({delimiter: '\r\n'}), () => {
@@ -51,20 +50,20 @@ function firmware() {
     nmeaParser.on("data", async (msg) => {
         if (msg.match(/^\$GNGGA,+/m)) {
             msg = msg.split(',')
-            let request = await http.request(
-                config.get('gw')
-                + '?nTime=' + msg[1]
-                + '&lat=' + msg[2]
-                + '&NS=' + msg[3]
-                + '&lon=' + msg[4]
-                + '&EW=' + msg[5]
-                + '&r_timestamp=' + TIMESTAMP
-                + '&roverID=' + config.get('roverID')
-            )
-            request.on('error', () => {
-                console.log('Error with connection to investigator via ' + config.get('gw'))
-            })
-            request.end()
+            // let request = await http.request(
+            //     config.get('gw')
+            //     + '?nTime=' + msg[1]
+            //     + '&lat=' + msg[2]
+            //     + '&NS=' + msg[3]
+            //     + '&lon=' + msg[4]
+            //     + '&EW=' + msg[5]
+            //     + '&r_timestamp=' + TIMESTAMP
+            //     + '&roverID=' + config.get('roverID')
+            // )
+            // request.on('error', () => {
+            //     console.log('Error with connection to investigator via ' + config.get('gw'))
+            // })
+            // request.end()
         }
     })
 }
