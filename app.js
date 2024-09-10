e751f9e2f9a3ad4e2b4cc22f31d9eb5fc6ea7fa2
Tolstov Ivan <doc830@icloud.com>
2024-09-10 10:18:37 +0300

    add oled

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const config = require('config')
 const cors = require('cors')
 const server = express()
 const firmware = require('./firmware/firmware')
+const oled = require('./firmware/oled')
 async function initialization () {
     await server.listen(config.get('port'))
 }
 initialization().then(()=>{
     console.log('Server started successfully on port ' + config.get('port') + '!')
     firmware()
+    oled()
 }).catch((err)=>{
     console.error('Internal server error', err)
     process.exit(1)
--- firmware/oled.js
+++ firmware/oled.js
@@ -1,29 +1,31 @@
 const i2c = require('i2c-bus')
 const Oled = require('oled-i2c-bus')
 const font = require('oled-font-5x7')
-
-// Открываем шину I2C
-const i2cBus = i2c.openSync(1); // Убедитесь, что используется правильная I2C шина
+function oled() {
+    // Открываем шину I2C
+    const i2cBus = i2c.openSync(1); // Убедитесь, что используется правильная I2C шина
 
 // Настройки дисплея
-const opts = {
-    width: 128,
-    height: 64,
-    address: 0x3C // Используйте адрес дисплея без учёта операций (в данном случае 0x78 -> 0x3C)
-};
+    const opts = {
+        width: 128,
+        height: 64,
+        address: 0x3C // Используйте адрес дисплея без учёта операций (в данном случае 0x78 -> 0x3C)
+    };
 
 // Создаем объект дисплея
-const oled = new Oled(i2cBus, opts);
+    const oled = new Oled(i2cBus, opts);
 
 // Инициализация дисплея
-oled.clearDisplay(); // Очистка экрана
-oled.turnOnDisplay(); // Включаем дисплей
+    oled.clearDisplay(); // Очистка экрана
+    oled.turnOnDisplay(); // Включаем дисплей
 
 // Отображение текста на экране
-oled.setCursor(1, 1); // Установка курсора в координаты (x, y)
-oled.writeString(font, 1, 'Hello, I2C Display!', 1, true); // Пишем текст
+    oled.setCursor(1, 1); // Установка курсора в координаты (x, y)
+    oled.writeString(font, 1, 'Hello, I2C Display!', 1, true); // Пишем текст
 
 // Делаем задержку перед очисткой экрана
-setTimeout(() => {
-    oled.clearDisplay(); // Очищаем экран через 5 секунд
-}, 5000);
+    setTimeout(() => {
+        oled.clearDisplay(); // Очищаем экран через 5 секунд
+    }, 5000);
+}
+module.exports = oled
