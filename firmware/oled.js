12271f33e053a45b7358d9f1db4a037bf2b2ff07
Tolstov Ivan <doc830@icloud.com>
2024-09-10 11:54:13 +0300

    add oled

--- firmware/oled.js
+++ firmware/oled.js
@@ -1,4 +1,3 @@
-const {SerialPort} = require('serialport')
 const i2c = require('i2c-bus')
 const Oled = require('oled-i2c-bus')
 const font = require('oled-font-pack')
@@ -10,14 +9,14 @@ function  oled() {
         bus: 2,
         driver:"SSD1306"
     }
-    const i2cBus = i2c.open(opts.bus)
-    let oled = new Oled(i2cBus, opts)
-    setInterval(()=> {
-        oled.clearDisplay(true)
-        oled.setCursor(20, Math.floor(64 / 2) +5)
-        oled.writeString(font.oled_5x7, 2, ' LOGGER', 1, true)
-
-    }, 1000)
+    //const i2cBus = i2c.openSync(opts.bus)
+    // let oled = new Oled(i2cBus, opts)
+    // setInterval(()=> {
+    //     oled.clearDisplay(true)
+    //     oled.setCursor(20, Math.floor(64 / 2) +5)
+    //     oled.writeString(font.oled_5x7, 2, ' LOGGER', 1, true)
+    //
+    // }, 1000)
 
 }
 module.exports = oled
