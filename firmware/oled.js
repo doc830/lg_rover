338a1d652bd6fc21864a80da89ee15f39eb6f6dc
Tolstov Ivan <doc830@icloud.com>
2024-09-10 10:36:53 +0300

    add oled

--- firmware/oled.js
+++ firmware/oled.js
@@ -1,6 +1,8 @@
+const {SerialPort} = require('serialport')
 const i2c = require('i2c-bus')
 const Oled = require('oled-i2c-bus')
 const font = require('oled-font-5x7')
+
 function oled() {
     // Открываем шину I2C
     const i2cBus = i2c.openSync(2); // Убедитесь, что используется правильная I2C шина
@@ -11,21 +13,25 @@ function oled() {
         height: 64,
         address: 0x3C // Используйте адрес дисплея без учёта операций (в данном случае 0x78 -> 0x3C)
     };
-
 // Создаем объект дисплея
     const oled = new Oled(i2cBus, opts);
+    const port = new SerialPort('/dev/ttyS1', {
+        baudRate: 115200, // Установите нужную скорость передачи данных
+        encoding: 'utf8'
+    })
+    // Обработка данных из UART
+    port.on('data', (data) => {
+        console.log('Получено сообщение:', data.toString()); // Выводим данные в консоль
+        oled.clearDisplay(); // Очищаем дисплей перед выводом нового текста
+        oled.setCursor(1, 1); // Установка курсора в координаты (x, y)
+        oled.writeString(font, 1, data.toString(), 1, true); // Пишем текст на дисплей
+    });
 
-// Инициализация дисплея
-    oled.clearDisplay(); // Очистка экрана
-    oled.turnOnDisplay(); // Включаем дисплей
+// Обработка ошибок
+    port.on('error', (err) => {
+        console.error('Ошибка UART:', err.message);
+    });
 
-// Отображение текста на экране
-    oled.setCursor(1, 1); // Установка курсора в координаты (x, y)
-    oled.writeString(font, 1, 'Hello, I2C Display!', 1, true); // Пишем текст
 
-// Делаем задержку перед очисткой экрана
-    setTimeout(() => {
-        oled.clearDisplay(); // Очищаем экран через 5 секунд
-    }, 5000);
 }
 module.exports = oled
