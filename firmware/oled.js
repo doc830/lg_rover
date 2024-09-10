b96fd884aab73356a08e004e7d1664f133564bf4
Tolstov Ivan <doc830@icloud.com>
2024-09-10 13:59:37 +0300

    add oled

--- firmware/oled.js
+++ firmware/oled.js
@@ -1,22 +1,48 @@
 'use strict'
 const i2c = require('i2c-bus')
-const Oled = require('oled-i2c-bus')
-const font = require('oled-font-pack')
+const i2cBus = i2c.openSync(2)
+const OLED_ADDRESS = 0x3
 function  oled() {
-    const opts = {
-        width: 128,
-        height: 64,
-        address: 0x3C,
-        bus: 2,
-        driver: "SSD1306"
-    }
-    const i2cBus = i2c.openSync(opts.bus)
-    let oled = new Oled(i2cBus, opts)
-    oled.clearDisplay()
-    setInterval(()=> {
+    initDisplay()
+    clearDisplay()
 
-
-    }, 1)
-
+}
+function sendCommand(command) {
+    i2cBus.writeByteSync(OLED_ADDRESS, 0x00, command); // 0x00 указывает, что это команда
+}
+function sendData(data) {
+    // Отправка массива данных (например, для вывода пикселей на дисплей)
+    i2cBus.writeI2cBlockSync(OLED_ADDRESS, 0x40, data.length, Buffer.from(data));
+}
+function clearDisplay() {
+    const emptyData = new Array(128 * 64 / 8).fill(0); // 128x64 пикселя, 1 бит на пиксель
+    sendData(emptyData);
+}
+function initDisplay() {
+    sendCommand(0xAE); // Display OFF
+    sendCommand(0xD5); // Set display clock divide ratio/oscillator frequency
+    sendCommand(0x80); // Suggested value
+    sendCommand(0xA8); // Set multiplex ratio
+    sendCommand(0x3F); // 64MUX
+    sendCommand(0xD3); // Set display offset
+    sendCommand(0x00); // No offset
+    sendCommand(0x40); // Set display start line to 0
+    sendCommand(0x8D); // Charge pump
+    sendCommand(0x14); // Enable charge pump
+    sendCommand(0x20); // Memory addressing mode
+    sendCommand(0x00); // Horizontal addressing mode
+    sendCommand(0xA1); // Set segment re-map to normal
+    sendCommand(0xC8); // Set COM output scan direction to remapped
+    sendCommand(0xDA); // Set COM pins hardware configuration
+    sendCommand(0x12); // Alternative COM pin configuration
+    sendCommand(0x81); // Set contrast control
+    sendCommand(0x7F); // Contrast value
+    sendCommand(0xD9); // Set pre-charge period
+    sendCommand(0xF1); // Pre-charge period value
+    sendCommand(0xDB); // Set VCOMH deselect level
+    sendCommand(0x40); // VCOMH deselect level
+    sendCommand(0xA4); // Resume RAM content display
+    sendCommand(0xA6); // Normal display (not inverted)
+    sendCommand(0xAF); // Display ON
 }
 module.exports = oled
