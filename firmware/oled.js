228feb756bd650462ee7e583e32819cf9a4aef1f
Tolstov Ivan <doc830@icloud.com>
2024-09-10 11:58:58 +0300

    add oled

--- firmware/oled.js
+++ firmware/oled.js
@@ -5,13 +5,14 @@ function  oled() {
     const opts = {
         width: 128,
         height: 64,
-        address: 0x30,
+        address: 0x3C,
         bus: 2,
         driver:"SSD1306"
     }
     const i2cBus = i2c.openSync(opts.bus)
     let oled = new Oled(i2cBus, opts)
     oled.clearDisplay(true)
+    oled.turnOffDisplay()
     // setInterval(()=> {
     //     oled.setCursor(20, Math.floor(64 / 2) +5)
     //     oled.writeString(font.oled_5x7, 2, ' LOGGER', 1, true)
