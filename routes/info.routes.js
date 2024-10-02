078c8b602541d67408dd5d7569889b91ba210db1
Tolstov Ivan <doc830@icloud.com>
2024-10-02 13:28:15 +0300

    Add info

--- routes/info.routes.js
+++ routes/info.routes.js
@@ -24,9 +24,9 @@ router.get('/battery', (req, res) => {
         let charge = Buffer.from([received[1]])
         let param = Buffer.from([received[2]])
         res.json({
-            "header": header.toString(),
-            "charge": charge.toString(),
-            "param ": param.toString()
+            "header": header.readUInt16BE(0),
+            "charge": charge.readUInt16BE(0),
+            "param ": param.readUInt16BE(0)
         })
         serialPort.close()
         res.end()
