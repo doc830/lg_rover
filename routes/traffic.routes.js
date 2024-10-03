d018e0de3421e669e235f190afb96f4e396287a3
Ivan Tolstov <doc830@icloud.com>
2024-10-03 18:28:54 +0300

    add visibility data

--- routes/traffic.routes.js
+++ routes/traffic.routes.js
@@ -2,9 +2,11 @@ const {Router} = require('express')
 const devices = require("../middleware/devices");
 const router = Router()
 router.get('/white', async (req, res) => {
-    await turn("A60301").then((response)=>{
-        res.json(response.command)
-        devices.serialPort2.close()
+    await turn("A60301").then(()=>{
+        let a = listen()
+        res.json({
+            a
+        })
     }).catch((err)=>{
         res.json({
             "err": "001",
@@ -38,7 +40,6 @@ router.get('/red', (req, res) => {
     res.end()
 })
 async function turn(command) {
-    let received = Buffer.alloc(0)
     await new Promise(async (resolve, reject) => {
         await devices.openPort({
             path: "/dev/ttyS1",
@@ -48,22 +49,24 @@ async function turn(command) {
             parity: "even"
         },2).then(async ()=>{
             await devices.sendMessage(Buffer.from(command, 'hex'), devices.serialPort2)
-            devices.serialPort2.on('data', (data)=> {
-                received = Buffer.concat([received,  Buffer.from(data, 'hex')])
-                let header = Buffer.from([received[0]]).readUInt8(0)
-                let command = Buffer.from([received[1]]).readUInt8(0)
-                let param = Buffer.from([received[2]]).readUInt8(0)
-                let response = {
-                    "header": header,
-                    "command": command,
-                    "light": param
-                }
-                resolve(response)
-            })
+            resolve()
         }).catch(err => {
             reject (new Error(err))
         })
     })
 }
-
+function listen() {
+    let received = Buffer.alloc(0)
+    devices.serialPort2.on('data', (data)=> {
+        received = Buffer.concat([received,  Buffer.from(data, 'hex')])
+        let header = Buffer.from([received[0]]).readUInt8(0)
+        let command = Buffer.from([received[1]]).readUInt8(0)
+        let param = Buffer.from([received[2]]).readUInt8(0)
+        return  ( {
+            "header": header,
+            "command": command,
+            "light": param
+        })
+    })
+}
 module.exports = router
