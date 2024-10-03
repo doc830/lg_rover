3ec286ff1cfd4e6700374f165666a31107289b66
Ivan Tolstov <doc830@icloud.com>
2024-10-03 18:21:42 +0300

    add visibility data

--- routes/traffic.routes.js
+++ routes/traffic.routes.js
@@ -2,27 +2,16 @@ const {Router} = require('express')
 const devices = require("../middleware/devices");
 const router = Router()
 router.get('/white', async (req, res) => {
-    let received = Buffer.alloc(0)
-    await turn("A60301").catch((err)=>{
+    await turn("A60301").then((response)=>{
+        res.json(response)
+    }).catch((err)=>{
         res.json({
             "err": "001",
             "info": err.message
         })
         res.end()
     })
-    devices.serialPort2.on('data', (data)=> {
-        received = Buffer.concat([received,  Buffer.from(data, 'hex')])
-        let header = Buffer.from([received[0]]).readUInt8(0)
-        let command = Buffer.from([received[1]]).readUInt8(0)
-        let param = Buffer.from([received[2]]).readUInt8(0)
-        res.json({
-            "header": header,
-            "command": command,
-            "param ": param
-        })
-        devices.serialPort2.close()
-        res.end()
-    })
+
 })
 router.get('/blue', (req, res) => {
     res.json({
@@ -49,6 +38,7 @@ router.get('/red', (req, res) => {
     res.end()
 })
 async function turn(command) {
+    let received = Buffer.alloc(0)
     await new Promise(async (resolve, reject) => {
         await devices.openPort({
             path: "/dev/ttyS1",
@@ -58,12 +48,23 @@ async function turn(command) {
             parity: "even"
         },2).then(async ()=>{
             await devices.sendMessage(Buffer.from(command, 'hex'), devices.serialPort2)
-            resolve()
+            devices.serialPort2.on('data', (data)=> {
+                received = Buffer.concat([received,  Buffer.from(data, 'hex')])
+                let header = Buffer.from([received[0]]).readUInt8(0)
+                let command = Buffer.from([received[1]]).readUInt8(0)
+                let param = Buffer.from([received[2]]).readUInt8(0)
+                devices.serialPort2.close()
+                let response = {
+                    "header": header,
+                    "command": command,
+                    "light": param
+                }
+                resolve(response)
+            })
         }).catch(err => {
             reject (new Error(err))
         })
     })
-
 }
 
 module.exports = router
