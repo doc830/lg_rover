268a12fb0ff4818a72142e75cd73dad12177e41a
Tolstov Ivan <doc830@icloud.com>
2024-10-04 12:12:21 +0300

    add visibility data

--- routes/weather.routes.js
+++ routes/weather.routes.js
@@ -11,7 +11,32 @@ router.get('/data', async (req, res) => {
         res.end()
     } else {
         await devices.sendMessage(Buffer.from('010300000031841E', 'hex'), devices.serialPort)
-            .then()
+            .then(()=>{
+                devices.serialPort.on('data', (data)=> {
+                    received = Buffer.concat([received,  Buffer.from(data, 'hex')])
+                    if (received.length ===  103) {
+                        let wind_direction = Buffer.from([received[5],received[6]])
+                        wind_direction = wind_direction.readUInt16BE(0)
+                        let wind_speed = Buffer.from([received[9],received[10],received[7],received[8]])
+                        wind_speed = wind_speed.readFloatBE(0)
+                        let temperature = Buffer.from([received[13],received[14],received[11],received[12]])
+                        temperature = temperature.readFloatBE(0)
+                        let humidity = Buffer.from([received[17],received[18],received[15],received[16]])
+                        humidity = humidity.readFloatBE(0)
+                        let pressure = Buffer.from([received[21],received[22],received[19],received[20]])
+                        pressure = pressure.readFloatBE(0)
+                        res.json({
+                            'wind_direction': wind_direction,
+                            'wind_speed': wind_speed,
+                            'temperature': temperature,
+                            'humidity': humidity,
+                            'pressure': pressure
+                        })
+                        res.end()
+                    }
+
+                })
+            })
             .catch((err)=>{
                 res.json({
                     "err": "001",
@@ -19,30 +44,7 @@ router.get('/data', async (req, res) => {
                 })
                 res.end()
             })
-        devices.serialPort.on('data', (data)=> {
-            received = Buffer.concat([received,  Buffer.from(data, 'hex')])
-            if (received.length ===  103) {
-                let wind_direction = Buffer.from([received[5],received[6]])
-                wind_direction = wind_direction.readUInt16BE(0)
-                let wind_speed = Buffer.from([received[9],received[10],received[7],received[8]])
-                wind_speed = wind_speed.readFloatBE(0)
-                let temperature = Buffer.from([received[13],received[14],received[11],received[12]])
-                temperature = temperature.readFloatBE(0)
-                let humidity = Buffer.from([received[17],received[18],received[15],received[16]])
-                humidity = humidity.readFloatBE(0)
-                let pressure = Buffer.from([received[21],received[22],received[19],received[20]])
-                pressure = pressure.readFloatBE(0)
-                res.json({
-                    'wind_direction': wind_direction,
-                    'wind_speed': wind_speed,
-                    'temperature': temperature,
-                    'humidity': humidity,
-                    'pressure': pressure
-                })
-                res.end()
-            }
 
-        })
     }
 
 })
