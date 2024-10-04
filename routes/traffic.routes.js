539998cf3a10bc5b3c3cae8d6a981416cfa41619
Tolstov Ivan <doc830@icloud.com>
2024-10-04 12:10:00 +0300

    add visibility data

--- routes/traffic.routes.js
+++ routes/traffic.routes.js
@@ -3,33 +3,28 @@ const devices = require("../middleware/devices");
 const router = Router()
 router.get('/white', async (req, res) => {
     let received = Buffer.alloc(0)
-    await turn("A604FF").then(()=>{}).catch((err)=>{
+    await turn("A60301").then(()=>{
+        devices.serialPort2.on('data', (data)=> {
+            received = Buffer.concat([received, Buffer.from(data, 'hex')])
+            let header = Buffer.from([received[0]]).readUInt8(0)
+            let code = Buffer.from([received[1]]).readUInt8(0)
+            let param = Buffer.from([received[2]]).readUInt8(0)
+            devices.serialPort2.close()
+            res.json({
+                "header": header,
+                "code": code,
+                "light ": param
+            })
+            res.end()
+        })
+    }).catch((err)=>{
         res.json({
             "err": "001",
             "info": err.message
         })
         res.end()
     })
-    await turn("A60301").then(()=>{}).catch((err)=>{
-        res.json({
-            "err": "001",
-            "info": err.message
-        })
-        res.end()
-    })
-    devices.serialPort2.on('data', (data)=> {
-        received = Buffer.concat([received, Buffer.from(data, 'hex')])
-        let header = Buffer.from([received[0]]).readUInt8(0)
-        let code = Buffer.from([received[1]]).readUInt8(0)
-        let param = Buffer.from([received[2]]).readUInt8(0)
-        devices.serialPort2.close()
-        res.json({
-            "header": header,
-            "code": code,
-            "light ": param
-        })
-        res.end()
-    })
+
 })
 
 async function turn(command) {
