0780060c6dd67c715f95666a1d4bc0f5ef3cb20d
Tolstov Ivan <doc830@icloud.com>
2024-10-07 11:29:01 +0300

    add weather station bug fix

--- routes/weather.routes.js
+++ routes/weather.routes.js
@@ -13,13 +13,16 @@ router.get('/data',  (req, res) => {
             devices.serialPort.on('data', (data)=> {
                 received = Buffer.concat([received,  Buffer.from(data, 'hex')])
                 if (received.length ===  103) {
-                    console.log(received)
-                    let wind_direction = received.readUInt16LE(5)
-                    let wind_speed = received.readFloatLE(7)
-                    let temperature = received.readFloatLE(11)
-                    let humidity = received.readFloatLE(15)
-                    let pressure = received.readFloatLE(19)
-                    devices.serialPort.removeAllListeners()
+                    let wind_direction = Buffer.from([received[5],received[6]])
+                    wind_direction = wind_direction.readUInt16BE(0)
+                    let wind_speed = Buffer.from([received[9],received[10],received[7],received[8]])
+                    wind_speed = wind_speed.readFloatBE(0)
+                    let temperature = Buffer.from([received[13],received[14],received[11],received[12]])
+                    temperature = temperature.readFloatBE(0)
+                    let humidity = Buffer.from([received[17],received[18],received[15],received[16]])
+                    humidity = humidity.readFloatBE(0)
+                    let pressure = Buffer.from([received[21],received[22],received[19],received[20]])
+                    pressure = pressure.readFloatBE(0)
                     res.json({
                         'wind_direction': wind_direction,
                         'wind_speed': wind_speed,
@@ -27,6 +30,7 @@ router.get('/data',  (req, res) => {
                         'humidity': humidity,
                         'pressure': pressure
                     })
+                    res.end()
                 }
             })
         }).catch((err)=>{
