fd145375e356d996d60a01b232e76bbf895af0a6
Tolstov Ivan <doc830@icloud.com>
2024-12-02 17:12:52 +0300

    Fix battery level dimension

--- routes/info.routes.js
+++ routes/info.routes.js
@@ -124,7 +124,7 @@ router.get('/battery',  (req, res) => {
                     received = {
                         "header": received.readUInt8(0),
                         "code": received.readUInt8(1),
-                        "charge": (received.readUInt8(2) >> 1)*0.042519+9
+                        "charge": ((received.readUInt8(2) >> 1)*0.042519+9).toFixed(2)
                     }
                     devices.closePort(2).then(()=>{
                         res.json(received)
