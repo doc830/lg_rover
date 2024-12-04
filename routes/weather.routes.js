9c8e8883070c9f5b29448c3bdb1068ab305fa782
Tolstov Ivan <doc830@icloud.com>
2024-12-04 19:09:44 +0300

    Weather station modbus debug

--- routes/weather.routes.js
+++ routes/weather.routes.js
@@ -24,17 +24,18 @@ router.get('/data',  (req, res) => {
                             "err": "002",
                             "info": '\'CRC mismatch: Received:\', receivedCRC, \'Calculated:\', calculatedCRC'
                         })
+                    } else {
+                        devices.serialPort.removeAllListeners()
+                        res.json({
+                            'wind_direction': Buffer.from([received[5],received[6]]).readUInt16BE(0),
+                            'wind_speed': Buffer.from([received[9],received[10],received[7],received[8]]).readFloatBE(0),
+                            'temperature': Buffer.from([received[13],received[14],received[11],received[12]]).readFloatBE(0),
+                            'humidity': Buffer.from([received[17],received[18],received[15],received[16]]).readFloatBE(0),
+                            'pressure': Buffer.from([received[21],received[22],received[19],received[20]]).readFloatBE(0),
+                            roverID: config.get('roverID'),
+                            "raw": received
+                        })
                     }
-                    devices.serialPort.removeAllListeners()
-                    res.json({
-                        'wind_direction': Buffer.from([received[5],received[6]]).readUInt16BE(0),
-                        'wind_speed': Buffer.from([received[9],received[10],received[7],received[8]]).readFloatBE(0),
-                        'temperature': Buffer.from([received[13],received[14],received[11],received[12]]).readFloatBE(0),
-                        'humidity': Buffer.from([received[17],received[18],received[15],received[16]]).readFloatBE(0),
-                        'pressure': Buffer.from([received[21],received[22],received[19],received[20]]).readFloatBE(0),
-                        roverID: config.get('roverID'),
-                        "raw": received
-                    })
                 }
             })
         }).catch((err)=>{
