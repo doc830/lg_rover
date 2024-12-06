48da76f26018848d81987e56b5d44458bdbbc6c7
Tolstov Ivan <doc830@icloud.com>
2024-12-06 13:43:39 +0300

    RS485 Hot plug

--- firmware/RS485.js
+++ firmware/RS485.js
@@ -16,7 +16,7 @@ const serialPortConfigWeather = {
     stopBits: 1,
     parity: "even"
 }
-const message = '010300000031841E'
+const message = Buffer.from('010300000031841E', 'hex')
 function rs485() {
     connectDevice().then((result)=>{
         if (result.device === 'weather') {
@@ -41,18 +41,21 @@ function connectDevice() {
             closePort(openedPort).then(()=>{
                 openPort(serialPortConfigWeather).then((port)=>{
                     sendMessage(port).then(()=>{
-                        resolve({
-                            device: 'weather',
-                            port
+                        listenPort(port).then(()=>{
+                            resolve({
+                                device: 'weather',
+                                port
+                            })
+                        }).catch((err)=>{
+                            closePort(port).then(()=>{
+                                reject(err)
+                            }).catch(err => {
+                                reject(err)
+                            })
                         })
                     }).catch((err)=>{
-                        closePort(port).then(()=>{
-                            reject(err)
-                        }).catch(err => {
-                            reject(err)
-                        })
+                        reject(err)
                     })
-
                 }).catch((err) => {
                     reject (err)
                 })
@@ -144,7 +147,6 @@ function listenPort(port) {
     })
 }
 function sendMessage(port) {
-    let received = Buffer.alloc(0)
     return new Promise((resolve, reject) => {
         port.write(Buffer.from('010300000031841E', 'hex'), err => {
             if (err) {
@@ -155,31 +157,6 @@ function sendMessage(port) {
                     return reject (new Error(err.message))
                 }
             })
-            port.on('data', (data) => {
-                clearTimeout(timeout)
-                received = Buffer.concat([received,  Buffer.from(data)])
-                if (received.length ===  103) {
-                    if (!CRC(received)) {
-                        received = recoverMessage(received)
-                    }
-                    port.removeAllListeners()
-                    return resolve ({
-                        wind_direction: Buffer.from([received[5],received[6]]).readUInt16BE(0),
-                        wind_speed: Buffer.from([received[9],received[10],received[7],received[8]]).readFloatBE(0),
-                        temperature: Buffer.from([received[13],received[14],received[11],received[12]]).readFloatBE(0),
-                        humidity: Buffer.from([received[17],received[18],received[15],received[16]]).readFloatBE(0),
-                        pressure: Buffer.from([received[21],received[22],received[19],received[20]]).readFloatBE(0),
-                        CRC: CRC(received),
-                        roverID: config.get('roverID'),
-                    })
-                } else {
-                    return reject (new Error('No valid data via RS485'))
-                }
-            })
-            let timeout = setTimeout(() => {
-                port.removeAllListeners()
-                reject (new Error ('Weather station does not respond'))
-            }, 1000)
         })
     })
 }
