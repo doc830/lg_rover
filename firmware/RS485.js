435f8d7bdc69548f3a7f9e44fb1e6019da24ee8a
Tolstov Ivan <doc830@icloud.com>
2024-12-09 12:35:06 +0300

    RS485 Hot plug

--- firmware/RS485.js
+++ firmware/RS485.js
@@ -108,7 +108,7 @@ function listenPort(port) {
                     received = recoverMessage(received)
                 }
                 port.removeAllListeners()
-                let wind_direction = Buffer.from([received[5],received[6]]).readUInt16BE(0)
+                let wind_direction = parseInt((Buffer.from([received[5],received[6]]).readUInt16BE(0)).toString())
                 if ( wind_direction >= 0 && wind_direction <= 360) {
                     reject (new Error ('Invalid weather data'))
                 }
