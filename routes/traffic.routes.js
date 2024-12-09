e7c1351e97f3064f7800af7eb3dfed59ce59079e
Tolstov Ivan <doc830@icloud.com>
2024-12-09 18:05:11 +0300

    add type ubx

--- routes/traffic.routes.js
+++ routes/traffic.routes.js
@@ -2,9 +2,10 @@ const {Router} = require('express')
 const firmware = require('../middleware/devices')
 const router = Router()
 let portAvailable = true
+let blink_flag = false
 router.get('/red', async (req, res) => {
     try {
-        await response(res, ["A60305"]);
+        await response(res, ["A60305"])
     } catch (err) {
         res.status(500).json({
             err: "001",
@@ -14,7 +15,7 @@ router.get('/red', async (req, res) => {
 })
 router.get('/yellow', async (req, res) => {
     try {
-        await response(res, ["A60304"]);
+        await response(res, ["A60304"])
     } catch (err) {
         res.status(500).json({
             err: "001",
@@ -24,7 +25,7 @@ router.get('/yellow', async (req, res) => {
 })
 router.get('/green', async (req, res) => {
     try {
-        await response(res, ["A60303"]);
+        await response(res, ["A60303"])
     } catch (err) {
         res.status(500).json({
             err: "001",
@@ -64,7 +65,7 @@ router.get('/yellow_green', async (req, res) => {
 })
 router.get('/yellow_2', async (req, res) => {
     try {
-        await response(res, ["A60304", "A60305"]) // Два желтых
+        await response(res, ["A60304", "A60305"])
     } catch (err) {
         res.status(500).json({
             err: "001",
@@ -72,9 +73,18 @@ router.get('/yellow_2', async (req, res) => {
         })
     }
 })
-// router.get('/yellow_blink', (req, res) => {
-//
-// })
+router.get('/yellow_blink', async (req, res) => {
+    try {
+        await response(res, ["A60304", "A60305"])
+        blink_flag = true
+        blink()
+    } catch (err) {
+        res.status(500).json({
+            err: "001",
+            info: "Не удалось обработать запрос: " + err.message
+        })
+    }
+})
 router.get('/off', async (req, res) => {
     try {
         await response(res, ["A604FF"])
@@ -96,6 +106,9 @@ async function response(res, commands) {
     let receives = []
     try {
         await turn("A604FF")
+        if (blink_flag) {
+            blink_flag = false
+        }
         for (const command of commands) {
             const result = await turn(command)
             receives.push(result.param)
@@ -146,5 +159,14 @@ function turn(command) {
             reject (new Error(err))
         })
     })
+}
+async function blink() {
+    await turn("A60405")
+    let timer = setTimeout(async ()=>{
+        await turn("A60305")
+        if (blink_flag) {
+            timer.refresh()
+        }
+    }, 1000)
 }
 module.exports = router
