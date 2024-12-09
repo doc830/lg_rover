712a9f3b8100917c527b6fdaf002c4ad4f30cdc6
Tolstov Ivan <doc830@icloud.com>
2024-12-09 19:04:47 +0300

    add type ubx

--- firmware/RS485.js
+++ firmware/RS485.js
@@ -27,6 +27,7 @@ function weatherService() {
             try {
                 await sendMessage(openedPort, weather_command)
                 const weather = await listenPort(openedPort)
+                console.log(weather)
                 postData(weather, "/api/rover/weather")
                 setTimeout(messaging, 2000)
             } catch (err) {
